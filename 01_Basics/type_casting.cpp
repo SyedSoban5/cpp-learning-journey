19b719ea18faca52fa69553b569ce41756da0f45
SYED SOBAN AHMED SHAH <syedsobanahmedshah1@gmail.com>
2025-10-18 05:02:35 -0700

    Create type_casting.cpp
    Explains type conversion between integer and float/double.

---
+++ 01_Basics/type_casting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 7;
+    double result;
+
+    result = (double)num / 2; // type casting int to double
+    cout << "Result after type casting: " << result << endl;
+
+    return 0;
+}
