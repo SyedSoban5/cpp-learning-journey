447a4739ba9ebbd3c3dc546270e3095d0a340c59
SYED SOBAN AHMED SHAH <syedsobanahmedshah1@gmail.com>
2025-10-18 05:19:29 -0700

    Create basic_function.cpp
    Introduces defining and calling a simple function.

---
+++ 03_Functions/basic_function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void displayMessage()
+{
+    cout << "Welcome to C++ Functions!" << endl;
+}
+
+int main()
+{
+    displayMessage(); // Function call
+    return 0;
+}
