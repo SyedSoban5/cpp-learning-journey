86c62201c569bb03049ab2b1afabecf3607e7498
SYED SOBAN AHMED SHAH <syedsobanahmedshah1@gmail.com>
2025-10-18 05:01:27 -0700

    Create variables_demo.cpp
    Demonstrates variable declaration, initialization, and data types.

---
+++ 01_Basics/variables_demo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 20;
+    float height = 5.9;
+    char grade = 'A';
+    string name = "Soban";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
