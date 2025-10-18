b2bd9a4c625245dd43deff5095175a0ef54dd5f2
SYED SOBAN AHMED SHAH <syedsobanahmedshah1@gmail.com>
2025-10-18 05:03:43 -0700

    Create comments_and_escape_sequences.cpp
    Introduces comments and escape characters.

---
+++ 01_Basics/comments_and_escape_sequences.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // This is a single-line comment
+    /*
+       This is a multi-line comment
+       explaining the use of escape sequences
+    */
+
+    cout << "Hello\tWorld!\n"; // \t adds a tab, \n adds a new line
+    cout << "This is \"C++\" programming!" << endl;
+
+    return 0;
+}
